9986bb34841b683b0a9c24a61b5881801ad97532
Luna DINAUT <ldinaut@e2r11p23.clusters.42paris.fr>
2022-02-09 16:58:11 +0100

    bref

--- checkmap.c
+++ checkmap.c
@@ -6,7 +6,7 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 11:43:21 by ldinaut           #+#    #+#             */
-/*   Updated: 2022/02/09 16:31:24 by ldinaut          ###   ########.fr       */
+/*   Updated: 2022/02/09 16:57:38 by ldinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,12 @@
 
 int	ft_checkstr1(char *str, int i)
 {
+	int	j;
 
+	j = 0;
+	if (str[j] == '1')
+	{
+	}
 }
 
 int	ft_check_map(int fd)
@@ -22,13 +27,28 @@ int	ft_check_map(int fd)
 	char 	**tab;
 	char 	*str;
 	int	i;
+	int	j;
 
 	i = 0;
+	j = 0;
+	str = get_next_line(fd);
+	while (str[j])
+	{
+		if (str[j] == '1')
+			j++;
+		else
+		{
+			ft_putstr_fd("Error\nMap  pas a la norme\n", 1);
+			return (0);
+		}
+
+	}
 	while (str != NULL)
 	{
-		str = get_next_line(fd);
 		i++;
-		if (ft_checkstr1(str, i));
+		if (ft_checkstr1(str, i))
+			return (1);
+		str = get_next_line(fd);
 
 	}
 	tab = malloc(sizeof(char *) * (i + 1));
