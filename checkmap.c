1ec7f8b8305c1eefd7dafbb9576bf2e63c1b670b
Luna DINAUT <ldinaut@e2r11p23.clusters.42paris.fr>
2022-02-09 16:35:17 +0100

    mdr

--- checkmap.c
+++ checkmap.c
@@ -6,11 +6,16 @@
 /*   By: ldinaut <ldinaut@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/09 11:43:21 by ldinaut           #+#    #+#             */
-/*   Updated: 2022/02/09 14:49:03 by ldinaut          ###   ########.fr       */
+/*   Updated: 2022/02/09 16:31:24 by ldinaut          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
+
+int	ft_checkstr1(char *str, int i)
+{
+
+}
 
 int	ft_check_map(int fd)
 {
@@ -18,5 +23,13 @@ int	ft_check_map(int fd)
 	char 	*str;
 	int	i;
 
-	
+	i = 0;
+	while (str != NULL)
+	{
+		str = get_next_line(fd);
+		i++;
+		if (ft_checkstr1(str, i));
+
+	}
+	tab = malloc(sizeof(char *) * (i + 1));
 }
